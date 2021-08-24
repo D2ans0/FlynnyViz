7490f3a75a2ff97ff6c4a82f5b1089aed5c4e345
D2ans0 <D2ans0@rambler.ru>
2021-08-24 18:56:39 +0300

    SD -> SoundCard, device display in -S
    - Audio capture should now work out of the box, no need for VA cables
    - Slight code refactor
    - Added the current capture device to the stats menu
    (maybe need to rename it to debug in future)

--- FlynnyViz.py
+++ FlynnyViz.py
@@ -3,11 +3,14 @@
 import os 
 import sys
 import argparse
-from time import time, sleep
+from time import time, sleep, strftime
 try:
 	import numpy as np
 	import cv2
+	import soundcard as sc
 except ImportError as error:
+	if error.name == "cv2":
+		sys.stdout.write(f'"{error.name}" is not installed, you can install it with:\npip install opencv-python\n')
 	sys.stdout.write(f'"{error.name}" is not installed, you can install it with:\npip install {error.name}\n')
 	exit()
 
@@ -27,7 +30,7 @@ def argument_parse(args=sys.argv[1:]):
 	return parser.parse_args(args)
 def main_args(group):
 	group.add_argument("-I", "--image", type=str, metavar='',
-		default=rf"{os.getcwd()}/img/Flynn.png", help="String with path to image")
+		default=os.path.join(os.getcwd(), "img", "Flynn.png"), help="String with path to image")
 	group.add_argument("-T", "--title", type=str, metavar='',
 		default="FlynnyViz", help="Name of the window (only accepts ASCII characters)")
 	group.add_argument("-F","--fps-cap", type=int, metavar='',
@@ -38,10 +41,12 @@ def main_args(group):
 		default=None, help="Apply grayscale mask image")
 def audio_args(group):
 	group.add_argument("-L", "--list-devices", action="store_true")
-	group.add_argument("-D", "--dampening", type=int, metavar='',
-		default=17, help="higher = less sensitivity to audio")
-	group.add_argument("-t", "--timing", type=int, metavar='',
-		default='6', help="Takes one capture every Nth frame and every N*2th frame to mix together")
+	group.add_argument("-D", "--dampening", type=float, metavar='',
+		default=1.5, help="higher = less sensitivity to audio")
+	group.add_argument("-B", "--boost", type=int, metavar='',
+		default=1, help="increase sensitivity")
+	group.add_argument("--smoothing", type=int, metavar='',
+		default='12', help="Takes one capture every Nth frame and every N*2th frame to mix together")
 	group.add_argument("-x1", "--weightx1", type=float, metavar='',
 		default=0.7,help="Weight of Nth frame")
 	group.add_argument("-x2", "--weightx2", type=float, metavar='',
@@ -78,28 +83,18 @@ def fps_calc(time_count_start, mode=""):	#1st var = fps, 2nd var = frame time
 		return frame_rate, frame_time
 
 def audio_stream():
-	global stream, sd, audio_sens
-	try:
-		from pyaudio import PyAudio, paInt16
-		stream = PyAudio().open(format=paInt16, channels=1, rate=4000, input=1, frames_per_buffer=64)
-		def audio_sens(audio_stream):	
-			data = np.frombuffer(audio_stream.read(64),dtype=np.int16)
-			data_merged = (np.max(data) - np.min(data))*1000/max_value
-			return data_merged
-	except ImportError:
-		try:
-			import sounddevice as sd
-			stream = sd.InputStream(dtype=np.int16 , channels=1, samplerate=4000, blocksize=64)
-			stream.start()
-			def audio_sens(audio_stream):
-				data = audio_stream.read(64)[0]
-				data_merged = (np.max(data) - np.min(data))*1000/max_value
-				return data_merged
-		except ImportError:
-			sys.stdout.write(
-				"""Neither pyaudio nor sounddevice were detected, try installing the latter with:
- pip install sounddevice""")
-			exit()
+	global stream, audio_sens
+	def_speaker = sc.default_speaker()
+	def_speaker = str(def_speaker)
+	def_speaker = def_speaker[def_speaker.find("("):def_speaker.find(")")+1]
+	mic = sc.get_microphone(def_speaker, include_loopback=True)
+	print(mic)
+	return mic
+
+def audio_sens(audio_stream):
+	data = audio_stream.record(numframes=16)
+	data_merged = (np.max(data) - np.min(data))*1000/dampen
+	return data_merged
 
 def scaling(image, percentage):
 	percentage = percentage / 100
@@ -130,19 +125,21 @@ def info_overlay(image, x, y, scale, face, color, info):
 	cv2.putText(image, f"fps: {info[0]} Frame time (ms): {(info[1]*100):.1f}", (x,y), face, scale, color)
 	cv2.putText(image, f"Shift: {info[2]} (Base: {info[3]})", (x,y*2), face, scale, color)
 	cv2.putText(image, f"Volume: Real: {info[4]:.0f} Smoothed: {info[5]:.0f}", (x,y*3), face, scale, color)
-	# print general info
-	#sys.stdout.write(f"{args.title}  \nfps: {info[0]}  \t\t(Frame time: {info[1]:.2f})  \t|\nShift: {info[2]}  \t\tBase: {info[3]}  \t\t|\nVolume: Real: {info[4]:.0f} \tSmoothed: {info[5]:.0f} \t\t|\n\x1B[4A")
+	cv2.putText(image, micname, (x,y*4), face, scale, color)
 
 def mainloop():
 	global iterator, volume_2, volume_3, info
+	volume_2, volume_3, iterator = 0, 0, 0
+	info = (0, 0, 0, 0, 0, 0)
+	args.fps_cap = 1/args.fps_cap #calculate nominal frame time
 	while cv2.getWindowProperty(args.title, cv2.WND_PROP_VISIBLE):	# mainloop, runs as long as the main window running
 		startTime0 = time()
 		iterator = (iterator + 1) % 180
 		volume_real = audio_sens(stream)
 
 		# smoothing
-		if not (iterator % args.timing): volume_2 = volume_real*args.weightx1
-		if not (iterator % args.timing*2): volume_3 = volume_real*args.weightx2
+		if not (iterator % args.smoothing): volume_2 = volume_real*args.weightx1
+		if not (iterator % args.smoothing*2): volume_3 = volume_real*args.weightx2
 		volume_smoothed = (volume_real + volume_2 + volume_3) / (1 + args.weightx1 + args.weightx2) #average out loudness
 
 		# calculate amount of hue shift and modify the channel
@@ -162,7 +159,6 @@ def mainloop():
 		else:
 			#alpha
 			img_out = apply_alpha(img_HSV2BGR, alpha)
-
 		# add info overlay
 		if args.stats: info_overlay(img_out, 5, 15, 0.4, cv2.FONT_HERSHEY_SIMPLEX, (255,255,255), info)
 
@@ -180,24 +176,22 @@ def mainloop():
 ### root ###
 if __name__ == "__main__":
 	args = argument_parse()
-	cv2.namedWindow(args.title) #init main window
-	audio_stream()
-	if args.list_devices: print('Available devices:\n', sd.query_devices()), exit()
 	#nothing to see here
 	if args.secret:
 		from stegano.lsb import reveal as dontlook
 		sys.stdout.write(dontlook("img/Flynn.png")), exit(0)
 	
+	cv2.namedWindow(args.title, flags=(cv2.WINDOW_GUI_NORMAL + cv2.WINDOW_AUTOSIZE)) #init main window
+	mic = audio_stream()
+	micname = str(mic)
+	
 	#variables setup
-	volume_2, volume_3, iterator = 0, 0, 0
-	info = (0, 0, 0, 0, 0, 0)
-	args.fps_cap = 1/args.fps_cap #calculate nominal frame time
+	dampen = 2**args.dampening
 	font_pos, font_scale, font_weight = (5, 15), 1, 1
 	font_color, font_Face = (255,255,255), cv2.FONT_HERSHEY_SIMPLEX
-	max_value = 2**args.dampening
 
-	img_source = open_image(args.image) 						#read image
-	if args.scale != 100: img_source = scaling(img_source, args.scale)	#scale on demand
+	img_source = open_image(args.image) #read image
+	if args.scale != 100: img_source = scaling(img_source, args.scale) #scale on demand
 	# check if image has alpha
 	if img_source.shape[2] == 4:
 		alpha = apply_alpha(img_source, alphaOnly=True)
@@ -214,6 +208,12 @@ if __name__ == "__main__":
 		if args.scale != 100: mask = scaling(mask, args.scale)
 		if mask.shape[2] >= 3: mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
 		masked = np.bitwise_or(mask, h)
-	#print(masked.shape)
-	#print(imgHSV.shape)
-	mainloop()
+
+	try:
+		with mic.recorder(samplerate=1000, blocksize=16) as stream:
+			mainloop()
+	except Exception as err:
+		with open('FlynnyViz.log', 'a') as log:
+			log.write(f"{repr(err)} {strftime('%d/%m/%Y %H:%M:%S')}\n")
+		with mic.recorder(samplerate=1000, blocksize=16) as stream:
+			mainloop()
